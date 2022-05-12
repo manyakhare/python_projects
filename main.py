17083373cede3fc45ac5e7b29d2accce84b4cb17
manyakhare <74348408+manyakhare@users.noreply.github.com>
2022-05-12 10:50:14 +0530

    turtle pong game

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
-import time
 from turtle import Screen
-from player import Player
-from car_manager import CarManager
+from paddle import Paddle
+from ball import Ball
 from scoreboard import Scoreboard
+import time
 
 screen = Screen()
-screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("Pong Game")
 screen.tracer(0)
 
-player = Player()
-car_manager = CarManager()
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+ball = Ball()
 scoreboard = Scoreboard()
 
 screen.listen()
-screen.onkey(player.go_up, "Up")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
 
-    car_manager.create_car()
-    car_manager.move_cars()
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
-    # Detect collision with car
-    for car in car_manager.all_cars:
-        if car.distance(player) < 20:
-            game_is_on = False
-            scoreboard.game_over()
+    # Detect collision with paddles
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
-    # Detect successful crossing
-    if player.is_at_finish_line():
-        player.go_to_start()
-        car_manager.level_up()
-        scoreboard.increase_level()
+    # detect if the ball is missed
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_paddle_won()
 
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_paddle_won()
 
 screen.exitonclick()
-
