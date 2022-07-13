cf4b86f87bfcf8874adbf2fe007b522af9be5ec9
manyakhare <74348408+manyakhare@users.noreply.github.com>
2022-07-13 22:53:38 +0530

    Add files via upload

--- scoreboard.py
+++ scoreboard.py
@@ -1,36 +1,27 @@
 from turtle import Turtle
-ALIGNMENT = 'center'
-FONTS = ('Arial', 18, 'normal')
+
+FONT = ("Courier", 24, "normal")
 
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        self.level = 1
         self.hideturtle()
-        self.goto(0, 270)
-        self.score = 0
-        with open("data.txt") as data:
-            self.high_score = int(data.read())
-        self.color("white")
+        self.penup()
+        self.goto(-280, 250)
         self.update_scoreboard()
 
     def update_scoreboard(self):
         self.clear()
-        self.write(f'Score : {self.score}  High Score : {self.high_score}', align=ALIGNMENT, font=FONTS)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
 
-    def reset(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
-            with open("data.txt", mode="w") as data:
-                data.write(f"{self.high_score}")
-        self.score = 0
+    def increase_level(self):
+        self.level += 1
         self.update_scoreboard()
 
-    # def game_over(self):
-    #     self.goto(0, 0)
-    #     self.write(f'Game Over', align=ALIGNMENT, font=FONTS)
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align="center", font=FONT)
 
-    def increase_score(self):
-        self.score += 1
-        self.update_scoreboard()
